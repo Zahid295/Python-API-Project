ea53b084e21008f611548faffff4da90d15c1a4e
Zahid295 <138613200+Zahid295@users.noreply.github.com>
2024-04-06 22:52:58 +0000

    Add view for home page

--- tech_project/tech_project/urls.py
+++ tech_project/tech_project/urls.py
@@ -19,5 +19,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('', include('tech_blog.urls')),
+    path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
 ]
