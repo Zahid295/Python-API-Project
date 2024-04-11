8128ae2dd8cca3c8bd0f258ccf298e62930ef572
Zahid295 <138613200+Zahid295@users.noreply.github.com>
2024-04-11 16:53:57 +0000

    Change settings file for allauth package

--- tech_project/settings.py
+++ tech_project/settings.py
@@ -41,7 +41,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount'
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
