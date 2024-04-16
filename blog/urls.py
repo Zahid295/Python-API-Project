860a781e1a73155f75e9d02621e279c2c3eb6a32
Malekulashter-K <157482554+Malekulashter-K@users.noreply.github.com>
2024-04-16 19:41:44 +0000

    add all setup to show posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.posts, name='posts'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
+
