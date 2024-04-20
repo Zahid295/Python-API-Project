87f6d31544c84d4ca5ae9a444039e468bde125bd
Zahid295 <138613200+Zahid295@users.noreply.github.com>
2024-04-20 14:26:03 +0000

    Add comments to urls file

--- blog/forms.py
+++ blog/forms.py
@@ -13,11 +13,3 @@ class PostUpdateForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content']
 
-#class PostUpdateView(UpdateView):
-#    model = Post
-#    fields = ['title', 'content', 'author']
-
-#    def form_valid(self, form):
-#        form.instance.author = self.request.user
-#        return super().form_valid(form)
-
